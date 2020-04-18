d444989d6811c43f05005c80be92ff0ccf4d95c7
Roberto Villegas <rvillegas@oxy.edu>
2020-04-18 00:20:11 -0700

    Added Start Class
    Added Start class with a test code to print a name and age.

---
+++ TestProjectForRepo/src/Start.java
@@ -0,0 +1,17 @@
+public class Start {
+    private String name;
+    private int age;
+
+    public Start(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public void turnToString(){
+        System.out.println("My name is " + this.name + " and I am " + this.age + " years old.");
+    }
+    public static void main(String[] args){
+        Start me = new Start ("Roberto Villegas", 19);
+        me.turnToString();
+    }
+}
