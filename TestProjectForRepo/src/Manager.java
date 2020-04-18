5eda187697ca028f66b2502160f6d15bc8326518
Roberto Villegas <rvillegas@oxy.edu>
2020-04-18 00:26:23 -0700

    Added Manager Class
    Added Manager class and edited Start class, not finished yet.

---
+++ TestProjectForRepo/src/Manager.java
@@ -0,0 +1,12 @@
+public class Manager extends Start{
+    private int storeID;
+
+    public Manager(String name, int age, int storeID){
+        super(name, age);
+        this.storeID = storeID;
+    }
+
+    public void printString(){
+        
+    }
+}
